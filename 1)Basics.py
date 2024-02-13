3169cc4da357905e9fc2aca2f91364f0e91b9cea
Eternal-Glitch <154918475+Eternal-Glitch@users.noreply.github.com>
2024-02-13 22:37:23 +0530

    Add files via upload

---
+++ 1)Basics.py
@@ -0,0 +1,30 @@
+# This is used only for comments and yeh that's it what else, what up nigga!!
+# ctrl + / will turn the selected lines into comments
+print("Hello you stupid fuck.",20)
+print(10*25, 30/10, 69+46, 83-99) 
+
+print("\nBTW this is how to \n get to a new line, pretty basic\n")
+
+print("\nHow to use \"double quote\" also single quote like nigg \' in a sentence coz if you just put it then python thinks \"WTF\", where is end where is start?")
+print('Or, you can just use "single quote (\')" to display quotes inside.')
+
+print("\nJoin us", 7,9, "Me too", 10, sep="~", end="Done bro")
+# escape sequence character is a characte followed by \ like \n \" etc
+'''
+This is a para ig about nothing at all.
+Nothing at all means ....
+These things are used to mark a multi line comment
+'''
+
+"""
+Guess what, even i work the same
+HAHAHAHhahHAHAHA, even i can comment multiple lines.
+"""
+
+#You can store paragraphs or poems like the comments you have written above in ''' or """ into a variable and print it.
+
+song = '''\n\nIt's just me myself and I,
+Solo ride until I die,
+Coz I, got me for life.\n
+'''
+print(song)
